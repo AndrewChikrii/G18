73201e20f3a4a3589c3d7cfc9343bf071a345386
Suspensium <vitaliysmorodinov1@gmail.com>
2022-05-30 23:27:16 +0300

    water and match logic added

--- Assets/Mechs/Matches/Match.cs
+++ Assets/Mechs/Matches/Match.cs
@@ -6,10 +6,18 @@ public class Match : MonoBehaviour
 {
     [SerializeField] float burnTime = 2f;
 
+    RaycastHit surfaceHit;
+
     void Start()
     {
         Destroy(gameObject, burnTime);
     }
 
-
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == "Water")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
