09a54b772353fe75915c3e232819564823671dbe
Suspense <55453335+Suspensium@users.noreply.github.com>
2022-05-30 16:36:50 +0300

    Nested wheel quest added

---
+++ Assets/Mechs/Misc/WheelNest.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelNest : MonoBehaviour
+{
+
+    [SerializeField] GameObject wheel;
+
+    [SerializeField] float dist;
+
+    private bool questDone;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        dist = Vector3.Distance(wheel.transform.position, transform.position);
+
+        if (dist < 0.5f && !questDone)
+        {
+            questDone = true;
+
+            wheel.transform.SetParent(transform);
+
+            GameObject.Find("PlayerCamera").GetComponent<PRaycast>().CancelAction();
+            wheel.transform.position = new Vector3(transform.position.x - 0.15f, transform.position.y, transform.position.z);
+            Destroy(wheel.GetComponent<Grabbable>());
+            Destroy(wheel.GetComponent<Rigidbody>());
+            wheel.GetComponent<Rotatable>().enabled = true;
+
+            wheel.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
