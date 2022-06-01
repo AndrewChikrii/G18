70593181bec5337da9334aa7295b70f171e95c93
AndrewChikrii <andrey.chikrii@gmail.com>
2022-06-01 18:47:39 +0300

    match prop dynamic fresnel

---
+++ Assets/Mechs/Matches/MatchFresnel.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchFresnel : MonoBehaviour
+{
+    GameObject player;
+    Renderer rend;
+    float dist;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        rend =  gameObject.GetComponent<Renderer>();
+    }
+
+    
+    void Update()
+    {
+        dist = Vector3.Distance(player.transform.position, transform.position);
+        if(dist < 5f) {
+            rend.sharedMaterial.SetFloat("_Fresnel_mult",  (5f - dist) / 5f);
+        }
+    }
+}
